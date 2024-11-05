954c0626c64fe8ea6ed9fee2dcc153a6914c6ca3
danjorgensen <upstart-deck-mace@duck.com>
2024-11-05 17:40:45 +0100

    feat(socket) wip

--- server/app.js
+++ server/app.js
@@ -105,4 +105,9 @@ app.use(pagesRouter);
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(PORT, (error) => {
+  if(error) {
+    return console.log("Internal Server Error", error);
+  }
+  return console.log("Server is running on port:", PORT)
+});
