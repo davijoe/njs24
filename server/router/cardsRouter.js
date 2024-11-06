7cd55c564d860292e06a767ac144ffc9e56aafc8
danjorgensen <upstart-deck-mace@duck.com>
2024-11-06 20:55:40 +0100

    feat(search) also moved new db client inside fetch card route

--- server/router/cardsRouter.js
+++ server/router/cardsRouter.js
@@ -3,30 +3,29 @@ import pkg from "pg";
 
 const { Client } = pkg;
 
-// Initialize PostgreSQL client
-const client = new Client({
-  user: process.env.PGUSER || "dan",
-  host: process.env.PGHOST || "localhost",
-  database: process.env.PGDATABASE || "hearthstone_db",
-  password: process.env.PGPASSWORD || "your_password",
-  port: process.env.PGPORT || 5432,
-});
-
 const router = express.Router();
 
 // Route to fetch card data
 router.get("/api/tests", async (req, res) => {
+  const client = new Client({
+    user: process.env.PGUSER || "dan",
+    host: process.env.PGHOST || "localhost",
+    database: process.env.PGDATABASE || "hearthstone_db",
+    password: process.env.PGPASSWORD || "your_password",
+    port: process.env.PGPORT || 5432,
+  });
+
   try {
-    await client.connect(); // Connect to db
+    await client.connect(); // Connec db
     const result = await client.query(
       "select * from hearthstone_cards where card_set='Battlegrounds' limit 1000",
     );
-    res.json(result.rows);
+    res.json(result.rows); // res as json
   } catch (error) {
     console.error("Error fetching card data:", error);
     res.status(500).json({ error: "Failed to fetch card data" });
   } finally {
-    await client.end(); // Close connection after query
+    await client.end(); // close after query
   }
 });
 
